543c54a77ccb268ec9202ceb9bb1dd7bb210005a
Juan COYLA <jcoyla123@gmail.com>
2020-07-25 23:26:03 +0200

    chore: add types container in pokemon detailsé

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -6,8 +6,22 @@ const PokemonDetails = (props) => {
   console.log(props.location.state);
 
   return (
-    <div>
-      details
+    <div className='PokemonDetails'>
+      <div className='avatar'>
+        <img src='' alt='avatar'/>
+      </div>
+      <div className='caracter-info'>
+        <div className='caracter-name'>name</div> 
+        <div className='caracter-number'> #12</div> 
+      </div>
+      <div className='types'>
+        <span className='logo'>
+          <img src='/img/leaf.png' alt=''/>
+        </span>
+        <span className='logo'>
+          <img src='/img/poison.png' alt=''/>
+        </span>
+      </div>
     </div>
   )
 }
