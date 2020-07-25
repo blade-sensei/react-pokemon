fcd4de20606c48f48ce3e29c1fb09624843b4e8e
Juan COYLA <jcoyla123@gmail.com>
2020-07-25 16:33:00 +0200

    chore: add scrolling to mobile-body container

--- src/components/ListPokemon.js
+++ src/components/ListPokemon.js
@@ -2,9 +2,44 @@ import React from 'react';
 import './ListPokemon.css'
 
 const ListPokemon = () => {
+  const pokemons = [
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    {'name': 'pikachu'},
+    {'name': 'bulbazor'},
+    
+  ];
+  const renderListPokemon = (pokemons) => (
+    pokemons.map((pokemon) => (
+      <div className='pokemon-overview'>
+        this is pokemon { pokemon.name }
+      </div>
+    ))
+  );
+  
   return (
-    <div>
-      List
+    <div className='ListPokemon'>
+      { renderListPokemon(pokemons) }
     </div>
   )
 }
